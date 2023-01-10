8089a706c51033d6f510f76e17edd300f42eab4a
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-10 14:25:33 -0300

    The main map size of the game is now dynamic

--- src/pac_man.c
+++ src/pac_man.c
@@ -5,7 +5,8 @@ int
 main(void)
 {
 	FILE *f;
-	char map[5][11];
+	unsigned int rows, columns;
+	char **map;
 
 	f = fopen("map.txt", "r");
 	if (f == 0)
@@ -13,12 +14,24 @@ main(void)
 		printf("There was an error\n");
 		exit(1);
 	}
+
+	fscanf(f, "%d %d", &rows, &columns);
+	map = malloc(sizeof(char *) * rows);
 	
-	for(register int i = 0; i < 4; i++)
+	for(register int i = 0; i < 5; i++)
+		*(map + i) = malloc(sizeof(char) * columns);
+
+	for(register int i = 0; i < 5; i++)
 		fscanf(f, "%s", map[i]);
 
-	for(register int i = 0; i < 4; i++)
+	fclose(f);
+
+	for(register int i = 0; i < 5; i++)
 		printf("%s\n", map[i]);
+
+	for(register int i = 0; i < 5; i++)
+		free(*(map + i));
+	free(map);
 
 	return 0;
 }
